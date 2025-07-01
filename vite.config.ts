f1682054074c78b93a3857e199f015dde613e18e
liushengkai <liushengkai95@gmail.com>
2025-07-01 16:47:26 +0800

    chore： css in js

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import { name as packageName } from './package.json';
 import vue from '@vitejs/plugin-vue';
+// use vite-plugin-css-injected-by-js 解决打包之后没有js引入css， 导致使用时还需要用户去import的问题
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cssInjectedByJsPlugin()],
   build: {
     lib: {
       name: packageName,
